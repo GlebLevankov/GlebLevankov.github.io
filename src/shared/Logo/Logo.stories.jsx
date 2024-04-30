8818d87d8e68e3364260522dc189bbb04db3dc3e
GlebLevankov <77899328+GlebLevankov@users.noreply.github.com>
2024-04-30 15:10:30 +0300

    Добавил Logo

---
+++ src/shared/Logo/Logo.stories.jsx
@@ -0,0 +1,11 @@
+import Logo from '../Logo/Logo';
+
+export default {
+  title: 'Logo',
+  component: Logo,
+};
+
+const Template = (arg) => <Logo {...arg} />;
+
+export const DefaultLogo = Template.bind({});
+DefaultLogo.args = {};
